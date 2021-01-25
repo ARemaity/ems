a01ce7be997a6415fc3b52c51b66264a45e82d4e
ARemaity <aliremaitybook@gmail.com>
2021-01-25 19:42:27 +0200

    change test.php

--- test.php
+++ test.php
@@ -4,5 +4,12 @@ require_once 'include/DB_manage.php';
 require_once 'include/DB_Reports.php';
 $db=new DB_Manage();
 $rpt=new DB_Reports();
+// example 
 
+$insertinto=$db->insertintoclient("ali","sour",700324234);
+if($insertinto){
+    echo "good";
+}else{
+    echo "bad";
+}
 ?>
